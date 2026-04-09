cf844c1227fab28fd186c3271285494db6804658
MailyRamos <maily.ramos@aluno.senai.br>
2026-04-09 10:32:43 -0300

    finalizado

--- js/models.js
+++ js/models.js
@@ -1,3 +1,4 @@
+
 let modelsJson = [
     // Objeto 1
     {
@@ -8,27 +9,24 @@ let modelsJson = [
         sizes: ['1/72', '1/48', '1/32'],
         description: 'Caça utilizado nos primeiros anos de combate no pacífico pela marinha dos EUA'
     },
-
     // Objeto 2
     {
-        id: 1,
+        id: 2,
         name: 'Focke Wulf Fw190 A5',
-        img: 'img/FwFWildcat.png',
+        img: 'img/Fw190A5.png',
         price: [80.00, 150.00, 200.00],
         sizes: ['1/144', '1/48', '1/32'],
-        description: 'Caça de motor em estrelada Luftwaffe, temido em médias e baixas altitudes'
-    },    
-
+        description: 'Caça de motor em estrela da Luftwaffe, temido em médias e baixas altitudes'
+    },
     // Objeto 3
     {
         id: 3,
-        name: 'Ki 61 - Hien',
-        img: 'img/Ki61Hien.png',
+        name: 'ki 61 - Hien',
+        img: 'img/ki61Hien.png',
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
-        description: 'Caça fabricado pela Kawasaki para o exercito japonês, com motor em linha'
-    },    
-
+        description: 'Caça fabricado pela Kawasaki para o exército japonês, com motor em linha'
+    },
     // Objeto 4
     {
         id: 4,
@@ -37,18 +35,16 @@ let modelsJson = [
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
         description: 'Famoso caça da Messerschmitt, voado pelos maiores ases da Luftwaffe, a versão G foi a mais produzida'
-    },    
-
+    },
     // Objeto 5
     {
         id: 5,
-        name: 'P47 D - Thunderbolt',
-        img: 'img/P47D.png',
+        name: 'p47 D - Thunderbolt',
+        img: 'img/p47D.png',
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
-        description: 'Caça bombardeio de fabricação americana utilizado pela FAB'
-    },    
-
+        description: 'Caça bombardeiro de fabricação americana utilizado pela FAB'
+    },
     // Objeto 6
     {
         id: 6,
@@ -56,9 +52,8 @@ let modelsJson = [
         img: 'img/P51D.png',
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
-        description: 'O mais conhecido caça americano, importante escolta para os bombardeiros diurnos'
-    },    
-
+        description: 'O mais conhecido caça americano, importante escolta para os bombadeiros diurnos'
+    },
     // Objeto 7
     {
         id: 7,
@@ -67,8 +62,7 @@ let modelsJson = [
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
         description: 'O mais famoso caça britânico, utilizado em toda a guerra'
-    },    
-
+    },
     // Objeto 8
     {
         id: 8,
@@ -76,6 +70,6 @@ let modelsJson = [
         img: 'img/Zero.png',
         price: [100.00, 150.00, 200.00],
         sizes: ['1/72', '1/48', '1/32'],
-        description: 'O mais famoso caça da marinha japonesa, terror do pacífico nos anos inicias da guerra'
-    },    
-]
+        description: 'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iniciais da guerra'
+    },
+];
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let cart = [];
 let modalQt = 0;
 let key = 0;
 
-// Função auxiliar para selecionar um único elemento dom DOM
+// Função auxiliar para selecionar um único elemento do DOM
 const c = (el) => document.querySelector(el);
 
 // Função auxiliar para selecionar vários elementos do DOM
@@ -14,7 +14,7 @@ modelsJson.map((item, index) => {
     // Clonar o item do modelo
     let modelsItem = c('.models .models-item').cloneNode(true);
 
-    // Atualizar os dados do item do modelo clonado
+    // Atualizar os dados do item do modelo clonado 
     modelsItem.setAttribute('data-key', index);
     modelsItem.querySelector('.models-item--img img').src = item.img;
     modelsItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price[2].toFixed(3)}`;
@@ -47,20 +47,19 @@ modelsJson.map((item, index) => {
         setTimeout(() => {
             c('.modelsWindowArea').style.opacity = 1;
         }, 200);
-
     });
     c('.models-area').append(modelsItem);
 });
 
 // Função para fechar o modal
-function closeModal () {
+function closeModal() {
     c('.modelsWindowArea').style.opacity = 0;
     setTimeout(() => {
         c('.modelsWindowArea').style.display = 'none';
     }, 500);
 }
 
-// Adicionar eventos de clique para fechar o modal
+// Adicionar eventos de clique par fechar o modal
 cs('.modelsInfo--cancelButton, .modelsInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 });
@@ -74,8 +73,8 @@ c('.modelsInfo--qtmenos').addEventListener('click', () => {
 });
 
 c('.modelsInfo--qtmais').addEventListener('click', () => {
-    modalQt++;
-    c('.modelsInfo--qt').innerHTML = modalQt;
+        modalQt++;
+        c('.modelsInfo--qt').innerHTML = modalQt;
 });
 
 // Adicionar eventos de clique para selecionar o tamanho do modelo no modal
@@ -87,21 +86,21 @@ cs('.modelsInfo--size').forEach((size, sizeIndex) => {
     });
 });
 
-// Adicionar evento de clique para adicionar item ao carrinho
+// Adicionar evento de clique para adicionar o item ao carrinho
 c('.modelsInfo--addButton').addEventListener('click', () => {
 
     // Lógica para adicionar o item ao carrinho
     let size = parseInt(c('.modelsInfo--size.selected').getAttribute('data-key'));
-    let identifier = modelsJson[key].id + '@' + size;
+    let identifier = modelsJson[key].id +'@' + size;
     let locaId = cart.findIndex((item) => item.identifier == identifier);
     if (locaId > -1) {
         cart[locaId].qt += modalQt;
     } else {
-        cart.push({
+        cart.push ({
             identifier,
             id: modelsJson[key].id,
             size,
-            qt: modalQt
+            qt: modalQt        
         });
     }
     updateCart();
@@ -109,7 +108,7 @@ c('.modelsInfo--addButton').addEventListener('click', () => {
 });
 
 // Evento de clique para exibir o carrinho quando há itens no carrinho
-c('.menu-openner').addEventListener('click', ()=>{
+c('.menu-openner').addEventListener('click', ()=> {
     if(cart.length > 0){
         c('aside').style.left = '0';
     };
@@ -121,7 +120,7 @@ c('.menu-closer').addEventListener('click', ()=> {
 })
 
 // Evento de clique para finalizar a compra e limpar o carrinho
-c('.cart--finalizar').addEventListener('click', ()=>{
+c('.cart--finalizar').addEventListener('click', ()=> {
     cart = [];
     updateCart();
 })
@@ -130,20 +129,20 @@ c('.cart--finalizar').addEventListener('click', ()=>{
 function updateCart(){
     // Atualizar o número de itens exibido no ícone do carrinho no menu
     c('.menu-openner span').innerHTML = cart.length;
-    // Verificar se há itens no carrinho
+    // Verificar se há itens no carriho
     if(cart.length > 0){
         // Adicionar a classe 'show' para exibir a barra lateral do carrinho
         c('aside').classList.add('show');
         // Limpar o conteúdo atual da lista de itens no carrinho
         c('.cart').innerHTML = '';
 
-        // Inicializaão de variáveis para os totais
+        // Inicialização de variáveis para os totais 
         let subtotal = 0;
         let desconto = 0;
         let total = 0;
 
         // Iterar sobre os itens no carrinho
-        cart.map((itemCart, index)=>{
+        cart.map((itemCart, index)=> {
             // Encontrar o item correspondente nos dados dos modelos
             let modelItem = modelsJson.find((itemBD)=>itemBD.id == itemCart.id);
 
@@ -169,11 +168,11 @@ function updateCart(){
 
             // Atualizar os dados do item clonado do carrinho
             cartItem.querySelector('img').src = modelItem.img;
-            cartItem.querySelector('.cart--item--nome').innerHTML= `${modelItem.name} - ${modelItem.sizes[itemCart.size]}`;
+            cartItem.querySelector('.cart--item--nome').innerHTML = `${modelItem.name} - ${modelItem.sizes[itemCart.size]}`;
             cartItem.querySelector('.cart--item--qt').innerHTML = itemCart.qt;
 
             // Adicionar eventos de clique para diminuir e aumentar a quantidade do item no carrinho
-            cartItem.querySelector('.cart--item--qtmenos').addEventListener('click', ()=> {
+            cartItem.querySelector('.cart--item--qtmenos').addEventListener('click', () => {
                 if(itemCart.qt > 1){
                     itemCart.qt --;
                 } else {
@@ -181,7 +180,7 @@ function updateCart(){
                 }
                 updateCart();
             });
-            cartItem.querySelector('.cart--item--qtmais').addEventListener('click', ()=> {
+            cartItem.querySelector('.cart--item--qtmais').addEventListener('click', () => {
                 itemCart.qt ++;
                 updateCart();
             });
@@ -194,15 +193,16 @@ function updateCart(){
         desconto = subtotal * 0.1;
         total = subtotal - desconto
 
-        // Atualizar os elementos HTML com os valores de subtotal, desconto e total
+        // Atualizar os elementos HTML com os valores de subtotl, desconto e total
         c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
         c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
 
     } else {
-        // Remover a classe 'show' para ocultar a barra lateral do carrinho
+        // Remover a classe "show" para ocultar a barra lateral do carrinho
         c('aside').classList.remove('show');
         // Esconder a barra lateral movendo-a para fora da tela
         c('aside').style.left = '100vw';
     }
 };
+
